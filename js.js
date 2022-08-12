2b3063e6112bee869927830b818b50d50116d631
mironshoxhabr <110828771+mironshoxhabr@users.noreply.github.com>
2022-08-12 17:45:00 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,129 @@
+let arr = [
+    {
+        id: Math.random(),
+        name: 'Timur',
+        info: {
+            school: '235',
+            faculity: 'SMM'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Imran',
+        info: {
+            school: 'ne izvestno',
+            faculity: 'programming'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Aminjon',
+        info: {
+            school: '444',
+            faculity: 'Dizayn'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Muxammad',
+        info: {
+            school: '5555',
+            faculity: 'Backend'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Timur',
+        info: {
+            school: '235',
+            faculity: 'SMM'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Imran',
+        info: {
+            school: 'ne izvestno',
+            faculity: 'programming'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Aminjon',
+        info: {
+            school: '444',
+            faculity: 'Dizayn'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Muxammad',
+        info: {
+            school: '5555',
+            faculity: 'Backend'
+        },
+    },
+]
+
+let categories = [
+    {
+        course: ' SMM',
+        count: 0,
+        names : []
+    },
+    {
+        course: 'PROGRAMMING',
+        count: 0,
+        names : []
+    },
+    {
+        course: '     3DSMAX',
+        count: 0,
+        names : []
+    },
+    {
+        course: ' DIZAYN',
+        count: 0,
+        names : []
+    },
+    {
+        course: '   BACKEND',
+        count: 0,
+        names : []
+    },
+]
+
+for(let item of arr ){
+    for(let item2 of categories){
+        if(item.info.faculity.toUpperCase() === item2.course.toUpperCase().trim()){
+            item2.count++
+            item2.names.push(item.name)
+        }
+    }
+}
+
+console.log(categories);
